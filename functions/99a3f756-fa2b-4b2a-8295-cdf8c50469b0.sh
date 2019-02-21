530b97e762173558ac2d28fef290d5ddc0e40152
PTcrusher <edgarjcsantos@gmail.com>
2019-02-21 00:39:52 +0000

    There's no way to check if papirus was installed without python pip

--- functions/99a3f756-fa2b-4b2a-8295-cdf8c50469b0.sh
+++ functions/99a3f756-fa2b-4b2a-8295-cdf8c50469b0.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 GUID='99a3f756-fa2b-4b2a-8295-cdf8c50469b0'
 DESCRIPTION='Install Papirus'
-DEPENDS_ON=( )
+DEPENDS_ON=( '67952747-d79b-45fd-8678-62be0bebb822' )
 #https://github.com/PiSupply/PaPiRus
-if [ `pip show RPi.GPIO | wc -l` -eq 0 ]
+if [ `pip show papirus | wc -l` -eq 0 ]
 then
     echo "Before using PaPiRus, do not forget to enable the SPI and the I2C interfaces. \
         You can enable the SPI by typing sudo raspi-config at the command line and then \
